d22caeb99ad68541b815f8ded232642a3c8585b1
guno-kim <64324992+guno-kim@users.noreply.github.com>
2021-03-12 03:16:42 +0900

    client axios 주소 변경

--- client/src/components/pages/Problem/index.js
+++ client/src/components/pages/Problem/index.js
@@ -1,6 +1,6 @@
 import React,{useEffect,useState,useRef} from 'react'
 import styled from 'styled-components'
-import axios from 'axios'
+import axios from '../../../axios'
 import { useSelector } from 'react-redux'
 
 import { UpOutlined,DownOutlined } from '@ant-design/icons';
