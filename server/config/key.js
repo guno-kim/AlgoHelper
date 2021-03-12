72b3957442b8a211924aa44ae0e4f1a0d155cf19
guno-kim <64324992+guno-kim@users.noreply.github.com>
2021-03-12 23:43:39 +0900

    mongoURI 변경

--- server/config/key.js
+++ server/config/key.js
@@ -1,3 +1,4 @@
 module.exports={
-    mongoURI:"mongodb+srv://guno:1234@cluster0.isshw.mongodb.net/AlgoHelper?retryWrites=true&w=majority"
+    //mongoURI:"mongodb+srv://guno:1234@cluster0.isshw.mongodb.net/AlgoHelper?retryWrites=true&w=majority"
+    mongoURI:"mongodb://guno:1234@cluster0-shard-00-00.isshw.mongodb.net:27017,cluster0-shard-00-01.isshw.mongodb.net:27017,cluster0-shard-00-02.isshw.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-yqif02-shard-0&authSource=admin&retryWrites=true&w=majority"
 }
