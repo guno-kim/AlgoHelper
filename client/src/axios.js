f85f33e2e1dd01107650d7f73341e3ce7b79d8b5
guno-kim <64324992+guno-kim@users.noreply.github.com>
2021-03-13 04:21:19 +0900

    axios cookie 설정

--- client/src/axios.js
+++ client/src/axios.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 
 const axiosClient = axios.create({
-    baseURL: 'http://ec2-3-35-209-162.ap-northeast-2.compute.amazonaws.com'
+    baseURL: 'http://ec2-3-35-209-162.ap-northeast-2.compute.amazonaws.com',
+    withCredentials:true// cookie 받기 위해 설정
 })
 
 export default axiosClient;
