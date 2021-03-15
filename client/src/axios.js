14825da517a6ad57593b0183632bb039a92a8a13
guno-kim <64324992+guno-kim@users.noreply.github.com>
2021-03-16 02:12:28 +0900

    qwe

--- client/src/axios.js
+++ client/src/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const axiosClient = axios.create({
-    baseURL: 'http://ec2-3-35-209-162.ap-northeast-2.compute.amazonaws.com',
+    baseURL: 'http://api.algoheper.ga',
     withCredentials:true// cookie 받기 위해 설정
 })
 
