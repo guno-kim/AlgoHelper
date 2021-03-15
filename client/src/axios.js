8a5365e429062cfa6a461282c791080f5b619362
guno-kim <64324992+guno-kim@users.noreply.github.com>
2021-03-16 02:16:52 +0900

    spelling

--- client/src/axios.js
+++ client/src/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const axiosClient = axios.create({
-    baseURL: 'http://api.algoheper.ga',
+    baseURL: 'http://api.algohelper.ga',
     withCredentials:true// cookie 받기 위해 설정
 })
 
