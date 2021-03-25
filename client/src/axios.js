b1dec18522aa3432f0b7e6c1cf7ef57a62bae2ed
guno-kim <64324992+guno-kim@users.noreply.github.com>
2021-03-26 03:45:38 +0900

    1

--- client/src/axios.js
+++ client/src/axios.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 const axiosClient = axios.create({
     //baseURL: 'http://api.algohelper.ga',
     baseURL: process.env.REACT_APP_ServerDomain,
-    withCredentials:true// cookie 받기 위해 설정
+    withCredentials:true, // cookie 받기 위해 설정
 })
 
 export default axiosClient;
---
+++ server/start.sh
@@ -0,0 +1,2 @@
+cd home/
+echo 'start!!!'
